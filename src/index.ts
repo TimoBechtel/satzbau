a12ae715d5a5ffdf026b9948c59fe2fcbd90b2e3
Timo Bechtel <hi@timobechtel.com>
2021-03-27 20:40:53 +0100

    chore: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+export { adjective } from './adjective';
+export type { Adjective } from './adjective';
+export { generateArticle } from './article';
+export type { Article } from './article';
+export type { Gender, GrammaticalCase, GrammaticalNumber } from './declinable';
 export { noun } from './noun';
 export type { Noun } from './noun';
+export { number } from './number';
 export { sentence, template, text, writeList } from './textHelper';
 export type { SupportedTextTypes, Writable } from './textHelper';
-export { synonyms, variants } from './variants';
+export { synonyms, variantPicker, variants } from './variants';
