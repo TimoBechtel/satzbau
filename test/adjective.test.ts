708d69807727b8f81c6f610697af3860d871a7ec
Timo Bechtel <hi@timobechtel.com>
2021-03-27 18:10:15 +0100

    test(adjective): add more tests

--- test/adjective.test.ts
+++ test/adjective.test.ts
@@ -1,22 +1,157 @@
 import { adjective } from '../src/adjective';
 
 test('adjectives decline properly', () => {
-	// TODO: test properly
+	const niceCases = {
+		sg: {
+			definite: {
+				nominative: {
+					m: 'nette',
+					f: 'nette',
+					n: 'nette',
+				},
+				accusative: {
+					m: 'netten',
+					f: 'nette',
+					n: 'nette',
+				},
+				genitive: {
+					m: 'netten',
+					f: 'netten',
+					n: 'netten',
+				},
+				dative: {
+					m: 'netten',
+					f: 'netten',
+					n: 'netten',
+				},
+			},
+			indefinite: {
+				nominative: {
+					m: 'netter',
+					f: 'nette',
+					n: 'nettes',
+				},
+				accusative: {
+					m: 'netten',
+					f: 'nette',
+					n: 'nettes',
+				},
+				genitive: {
+					m: 'netten',
+					f: 'netten',
+					n: 'netten',
+				},
+				dative: {
+					m: 'netten',
+					f: 'netten',
+					n: 'netten',
+				},
+			},
+			none: {
+				nominative: {
+					m: 'netter',
+					f: 'nette',
+					n: 'nettes',
+				},
+				accusative: {
+					m: 'netten',
+					f: 'nette',
+					n: 'nettes',
+				},
+				genitive: {
+					m: 'netten',
+					f: 'netter',
+					n: 'netten',
+				},
+				dative: {
+					m: 'nettem',
+					f: 'netter',
+					n: 'nettem',
+				},
+			},
+		},
+		pl: {
+			// no gender differences in plural
+			definite: {
+				nominative: 'netten',
+				accusative: 'netten',
+				genitive: 'netten',
+				dative: 'netten',
+			},
+			indefinite: {
+				nominative: 'netten',
+				accusative: 'netten',
+				genitive: 'netten',
+				dative: 'netten',
+			},
+			none: {
+				nominative: 'nette',
+				accusative: 'nette',
+				genitive: 'netter',
+				dative: 'netten',
+			},
+		},
+	};
 
-	const red = adjective('rot');
-	expect(red.write()).toBe('rote');
-	expect(red.accusative().write()).toBe('rote');
-	expect(red.genitive().write()).toBe('roten');
-	expect(red.dative().write()).toBe('roten');
-	expect(red.plural().write()).toBe('roten');
+	const nice = adjective('nett');
+	let singularCaseCount = 0;
+	Object.entries(niceCases.sg).forEach(([article, values]) => {
+		Object.entries(values).forEach(([gCase, values]) => {
+			Object.entries(values).forEach(([gender, expectedWord]) => {
+				singularCaseCount++;
+				expect(
+					nice
+						.singular()
+						.article(article as any)
+						.gender(gender as any)
+						[gCase]()
+						.write()
+				).toBe(expectedWord);
+			});
+		});
+	});
+	expect(singularCaseCount).toBe(12 * 3);
 
-	const quite = adjective('leise');
-	expect(quite.write()).toBe('leise');
-	expect(quite.accusative().write()).toBe('leise');
-	expect(quite.genitive().write()).toBe('leisen');
-	expect(quite.dative().write()).toBe('leisen');
-	expect(quite.plural().write()).toBe('leisen');
+	let pluralCaseCount = 0;
+	Object.entries(niceCases.pl).forEach(([article, values]) => {
+		Object.entries(values).forEach(([gCase, expectedWord]) => {
+			pluralCaseCount++;
+			for (const gender of ['m', 'n', 'f']) {
+				expect(
+					nice
+						.plural()
+						.gender(gender as any)
+						.article(article as any)
+						[gCase]()
+						.write()
+				).toBe(expectedWord);
+			}
+		});
+	});
+	expect(pluralCaseCount).toBe(4 * 3);
+});
 
+test('handles edge cases', () => {
+	/**
+	 * we do not check for every form (case, number, gender, article)
+	 * as the word transformation for edge cases is done on adjective initialization
+	 * everything else stays the same as tested in previous test
+	 */
+
+	/**
+	 * words ending with -e should only contain a single e at the end
+	 * e.g. leise => leise instead of leise = leisee
+	 */
+	const quiet = adjective('leise');
+	expect(quiet.write()).toBe('leise');
+	expect(quiet.accusative().write()).toBe('leise');
+	expect(quiet.genitive().write()).toBe('leisen');
+	expect(quiet.dative().write()).toBe('leisen');
+	expect(quiet.plural().write()).toBe('leisen');
+
+	/**
+	 * words ending with -er should not contain an 'e'
+	 */
 	const pricey = adjective('teuer');
 	expect(pricey.write()).toBe('teure');
 	expect(pricey.accusative().write()).toBe('teure');
@@ -24,31 +159,23 @@ test('adjectives decline properly', () => {
 	expect(pricey.dative().write()).toBe('teuren');
 	expect(pricey.plural().write()).toBe('teuren');
 
+	/**
+	 * words ending with -el should switch 'e' with 'l'
+	 * e.g. dunkel => dunkle instead of dunkel => dunkele
+	 */
+	const dark = adjective('dunkel');
+	expect(dark.write()).toBe('dunkle');
+	expect(dark.accusative().write()).toBe('dunkle');
+	expect(dark.genitive().write()).toBe('dunklen');
+	expect(dark.dative().write()).toBe('dunklen');
+	expect(dark.plural().write()).toBe('dunklen');
+
+	/**
+	 * special case: "hoch"
+	 */
 	const high = adjective('hoch');
-	expect(high.write()).toBe('hohe');
+	expect(high.nominative().write()).toBe('hohe');
 	expect(high.accusative().write()).toBe('hohe');
 	expect(high.genitive().write()).toBe('hohen');
 	expect(high.dative().write()).toBe('hohen');
-	expect(high.plural().write()).toBe('hohen');
-
-	let adj = adjective('dunkel');
-	expect(adj.write()).toBe('dunkle');
-	expect(adj.accusative().write()).toBe('dunkle');
-	expect(adj.genitive().write()).toBe('dunklen');
-	expect(adj.dative().write()).toBe('dunklen');
-	expect(adj.plural().write()).toBe('dunklen');
-
-	adj = adj.gender('m').article('indefinite');
-	expect(adj.write()).toBe('dunkler');
-	expect(adj.accusative().write()).toBe('dunklen');
-	expect(adj.genitive().write()).toBe('dunklen');
-	expect(adj.dative().write()).toBe('dunklen');
-	expect(adj.plural().write()).toBe('dunklen');
-
-	adj = adj.gender('f').article('indefinite');
-	expect(adj.write()).toBe('dunkle');
-	expect(adj.accusative().write()).toBe('dunkle');
-	expect(adj.genitive().write()).toBe('dunklen');
-	expect(adj.dative().write()).toBe('dunklen');
-	expect(adj.plural().write()).toBe('dunklen');
 });
--- test/variants.test.ts
+++ test/variants.test.ts
@@ -1,4 +1,6 @@
+import { adjective } from '../src/adjective';
 import { noun } from '../src/noun';
+import { sentence } from '../src/textHelper';
 import { synonyms, variants } from '../src/variants';
 
 test('always picks a different variant', () => {
@@ -15,7 +17,7 @@ test('always picks a different variant', () => {
 	last = next;
 });
 
-test('allows creating declinable synonyms', () => {
+test('allows creating declinable synonyms for nouns', () => {
 	const light = synonyms(
 		noun('das licht, die lichter, des lichtes'),
 		noun('die lampe, die lampen, der lampe'),
@@ -26,4 +28,49 @@ test('allows creating declinable synonyms', () => {
 	expect(expected).toContain(light.dative().plural().write());
 	expect(expected).toContain(light.dative().plural().write());
 	expect(expected).toContain(light.dative().plural().write());
+});
+
+test('allows creating declinable synonyms for adjectives', () => {
+	const easy = synonyms(
+		adjective('leicht'),
+		adjective('simpel'),
+		adjective('einfach')
+	)
+		.genitive()
+		.plural();
+
+	const expected = ['leichten', 'simplen', 'einfachen'];
+
+	expect(expected).toContain(easy.write());
+	expect(expected).toContain(easy.write());
+	expect(expected).toContain(easy.write());
+});
+
+test('allows adding adjective synonyms to nouns', () => {
+	const easy = synonyms(
+		adjective('leicht'),
+		adjective('simpel'),
+		adjective('einfach')
+	)
+		.genitive()
+		.plural();
+	const task = synonyms(
+		noun('die aufgabe,-n,-'),
+		noun('der job,-s,-s')
+	).attributes(easy);
+
+	const expected = [
+		'Eine einfache Aufgabe.',
+		'Eine simple Aufgabe.',
+		'Eine leichte Aufgabe.',
+		'Ein leichter Job.',
+		'Ein simpler Job.',
+		'Ein einfacher Job.',
+	];
+	expect(expected).toContain(sentence`${task}`.write());
+	expect(expected).toContain(sentence`${task}`.write());
+	expect(expected).toContain(sentence`${task}`.write());
+	expect(expected).toContain(sentence`${task}`.write());
+	expect(expected).toContain(sentence`${task}`.write());
+	expect(expected).toContain(sentence`${task}`.write());
 });
